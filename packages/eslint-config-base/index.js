8f72e8f4dd1f4eb2278d8788c67eec84cc11fb34
Paul <p.simkevi@gmail.com>
2021-03-05 14:06:50 +0100

    bump ecmaVersion to 2020

--- packages/eslint-config-base/index.js
+++ packages/eslint-config-base/index.js
@@ -10,7 +10,7 @@ module.exports = {
   plugins: ["import", "unicorn", "babel"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: 2020,
     sourceType: "module",
   },
   settings: {
