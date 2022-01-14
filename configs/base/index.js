266e41bf248c406fb3792a1299c1d051657f152b
Paul <p.simkevi@gmail.com>
2022-01-14 19:52:41 +0200

    configs-base: disable `unicorn/prevent-abbreviations` for imports
    fixes #17

--- configs/base/index.js
+++ configs/base/index.js
@@ -161,6 +161,8 @@ module.exports = {
     "unicorn/prevent-abbreviations": [
       "error",
       {
+        checkDefaultAndNamespaceImports : false,
+        checkShorthandImports: false,
         replacements: {
           ref: false,
           refs: false,
