d51afadf5f8388db2a428265282db7e65991cfe1
Paul <p.simkevi@gmail.com>
2022-03-18 10:37:32 +0200

    react-config: update to match the new convention in the base config

--- configs/react/index.js
+++ configs/react/index.js
@@ -1,8 +1,8 @@
 module.exports = {
   extends: [
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    require.resolve("@mediamonks/eslint-config-base"),
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    require.resolve('@mediamonks/eslint-config-base'),
   ],
   parserOptions: {
     ecmaFeatures: {
@@ -11,55 +11,64 @@ module.exports = {
   },
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
   rules: {
-    "react/jsx-props-no-spreading": "off",
-    "react/react-in-jsx-scope": "off", // no longer needed with the new JSX transform
-    "react-hooks/exhaustive-deps": "error",
-    "react/button-has-type": "error",
-    "react/display-name": "off",
-    "react/jsx-boolean-value": "error",
-    "react/jsx-curly-brace-presence": ["error", { props: "never", children: "never" }],
-    "react/jsx-filename-extension": ["error", { extensions: [".jsx", ".tsx"] }],
-    "react/jsx-wrap-multilines": "off",
-    "react/no-array-index-key": "error",
-    "react/no-this-in-sfc": "error",
-    "react/no-unused-prop-types": "error",
-    "react/no-unused-state": "error",
-    "react/void-dom-elements-no-children": "error",
+    'react/jsx-props-no-spreading': 'off',
+    'react/react-in-jsx-scope': 'off', // no longer needed with the new JSX transform
+    'react-hooks/exhaustive-deps': 'error',
+    'react/button-has-type': 'error',
+    'react/display-name': 'off',
+    'react/jsx-boolean-value': 'error',
+    'react/jsx-curly-brace-presence': ['error', { props: 'never', children: 'never' }],
+    'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx'] }],
+    'react/jsx-wrap-multilines': 'off',
+    'react/no-array-index-key': 'error',
+    'react/no-this-in-sfc': 'error',
+    'react/no-unused-prop-types': 'error',
+    'react/no-unused-state': 'error',
+    'react/void-dom-elements-no-children': 'error',
   },
   overrides: [
     {
-      files: ["*.tsx"],
+      files: ['*.tsx'],
       rules: {
-        "react/prop-types": "off",
-        "@typescript-eslint/naming-convention": [
-          "error",
+        'react/prop-types': 'off',
+        '@typescript-eslint/naming-convention': [
+          'error',
           {
-            selector: "default",
-            format: ["strictCamelCase"],
-            leadingUnderscore: "forbid",
-            trailingUnderscore: "forbid",
+            selector: 'default',
+            format: ['strictCamelCase'],
+            leadingUnderscore: 'forbid',
+            trailingUnderscore: 'forbid',
           },
           {
-            selector: "typeLike",
-            format: ["StrictPascalCase"],
+            selector: 'objectLiteralProperty',
+            modifiers: ['requiresQuotes'],
+            format: null,
           },
           {
-            selector: "variable",
+            selector: 'typeLike',
+            format: ['StrictPascalCase'],
+          },
+          {
+            selector: 'function',
             // Exception for FunctionComponents
-            format: ["strictCamelCase", "StrictPascalCase", "UPPER_CASE"],
+            format: ['strictCamelCase', 'StrictPascalCase'],
           },
           {
-            selector: "function",
-            // Exception for FunctionComponents
-            format: ["strictCamelCase", "StrictPascalCase"],
+            selector: 'variable',
+            format: ['strictCamelCase', 'UPPER_CASE'],
           },
           {
-            selector: "enumMember",
-            format: ["StrictPascalCase"],
+            selector: 'variable',
+            modifiers: ['exported'],
+            format: ['strictCamelCase', 'StrictPascalCase', 'UPPER_CASE'],
+          },
+          {
+            selector: 'enumMember',
+            format: ['StrictPascalCase'],
           },
         ],
       },
