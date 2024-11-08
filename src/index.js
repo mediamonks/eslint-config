476a5bf5e5c659da4a5344cd9a428cd9c9d82e40
Leroy Korterink <leroy@mediamonks.com>
2024-11-08 11:07:21 +0100

    Add security, markdown and prettier configuration

--- src/index.js
+++ src/index.js
@@ -1,134 +1,150 @@
 import eslint from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
 import eslintPluginImport from 'eslint-plugin-import';
 import eslintPluginJsxA11y from 'eslint-plugin-jsx-a11y';
 import eslintPluginReact from 'eslint-plugin-react';
 import eslintPluginReactHooks from 'eslint-plugin-react-hooks';
+import eslintPluginSecurity from 'eslint-plugin-security';
 import eslintPluginStorybook from 'eslint-plugin-storybook';
 import eslintPluginUnicorn from 'eslint-plugin-unicorn';
 import typescriptEslint from 'typescript-eslint';
-import { recommended as javascriptRecommended } from './javascript.js';
-import { recommended as reactRecommended } from './react.js';
-import { recommended as typescriptRecommended } from './typescript.js';
-import { recommended as typescriptReactRecommended } from './typescriptReact.js';
+import { rules as javascriptRules } from './javascript.js';
+import { rules as reactRules } from './react.js';
+import { rules as typescriptRules } from './typescript.js';
+import { rules as typescriptReactRules } from './typescriptReact.js';
 
 /** @type {import('eslint').Linter.Config} */
 const javascript = [
   {
-    ...eslint.configs.recommended,
-    name: '@mediamonks/eslint-config / eslint recommended configuration',
-  },
-  {
-    ...eslintPluginImport.flatConfigs.recommended,
-    name: '@mediamonks/eslint-config / eslint-plugin-import / recommended configuration',
-  },
-  {
-    ...eslintPluginJsxA11y.flatConfigs.recommended,
-    name: '@mediamonks/eslint-config / eslint-plugin-jsx-a11y / recommended configuration',
-  },
-  {
-    ...eslintPluginUnicorn.configs['flat/recommended'],
-    name: '@mediamonks/eslint-config / eslint-plugin-unicorn / recommended configuration',
-  },
-  {
-    ...javascriptRecommended,
-    name: '@mediamonks/eslint-config / javascript recommended configuration',
+    plugins: {
+      unicorn: eslintPluginUnicorn,
+      import: eslintPluginImport,
+      'jsx-a11y': eslintPluginJsxA11y,
+    },
+    rules: {
+      ...eslint.configs.recommended.rules,
+      ...eslintPluginImport.flatConfigs.recommended.rules,
+      ...eslintPluginJsxA11y.flatConfigs.recommended.rules,
+      ...eslintPluginUnicorn.configs['flat/recommended'].rules,
+      ...javascriptRules,
+    },
+    name: '@mediamonks/eslint-config / javascript configuration',
   },
 ];
 
 /** @type {import('eslint').Linter.Config} */
 const react = [
   {
+    plugins: {
+      'react-hooks': eslintPluginReactHooks,
+      react: eslintPluginReact,
+      unicorn: eslintPluginUnicorn,
+      a11y: eslintPluginJsxA11y,
+    },
     settings: {
       react: {
         version: 'detect',
       },
     },
-    files: ['**/*.jsx', '**/*.tsx'],
-    name: '@mediamonks/eslint-config / react settings',
-  },
-  {
-    ...eslintPluginReact.configs.flat.recommended,
-    files: ['**/*.jsx', '**/*.tsx'],
-    name: '@mediamonks/eslint-config / eslint-plugin-react / recommended configuration',
-  },
-  {
-    plugins: {
-      'react-hooks': eslintPluginReactHooks,
+    rules: {
+      ...eslintPluginReactHooks.configs.recommended.rules,
+      ...eslintPluginReact.configs.flat.recommended.rules,
+      ...eslintPluginImport.flatConfigs.react.rules,
+      ...reactRules,
     },
-    ...eslintPluginReactHooks.configs.recommended,
-    files: ['**/*.jsx', '**/*.tsx'],
-    name: '@mediamonks/eslint-config / eslint-plugin-react-hooks / recommended configuration',
+    name: '@mediamonks/eslint-config / react configuration',
   },
   {
-    ...eslintPluginImport.flatConfigs.react,
-    files: ['**/*.jsx', '**/*.tsx'],
-    name: '@mediamonks/eslint-config / eslint-plugin-import / recommended react configuration',
+    files: ['**/routes/**/*'],
+    rules: {
+      'unicorn/filename-case': 'off',
+    },
+    name: '@mediamonks/eslint-config / react configuration / file based routing',
   },
-  ...eslintPluginStorybook.configs['flat/recommended'].map((config) => ({
-    ...config,
-    name: `@mediamonks/eslint-config / eslint-plugin-storybook / ${config.name}`,
-  })),
+];
+
+/** @type {import('eslint').Linter.Config} */
+const storybook = [
+  ...eslintPluginStorybook.configs['flat/recommended'],
   {
     rules: {
       'react/jsx-no-literals': 'off',
     },
     files: ['**/*.stories.@(ts|tsx|js|jsx|mjs|cjs)', '**/*.story.@(ts|tsx|js|jsx|mjs|cjs)'],
-    name: `@mediamonks/eslint-config / react storybook overrides`,
-  },
-  {
-    ...reactRecommended,
-    files: ['**/*.js', '**/*.mjs', '**/*.cjs', '**/*.jsx', '**/*.tsx'],
-    name: '@mediamonks/eslint-config / react recommended configuration',
+    name: `@mediamonks/eslint-config / storybook`,
   },
 ];
 
 /** @type {import('@typescript-eslint/utils').TSESLint.FlatConfig.ConfigFile} */
 const typescript = [
-  // Recommended plugin configurations
-  ...typescriptEslint.configs.strict.map((config) => ({
-    ...config,
-    files: ['**/*.ts', '**/*.tsx', '**/*.mts', '**/*.cts'],
-    name: `@mediamonks/eslint-config / ${config.name}`,
-  })),
+  ...typescriptEslint.configs.strict,
   {
-    ...eslintPluginImport.flatConfigs.typescript,
-    files: ['**/*.ts', '**/*.tsx', '**/*.mts', '**/*.cts'],
-    name: '@mediamonks/eslint-config / eslint-plugin-import / .ts configuration',
-  },
-  {
-    ...typescriptRecommended,
-    files: ['**/*.ts', '**/*.tsx', '**/*.mts', '**/*.cts'],
-    name: '@mediamonks/eslint-config / typescript recommended configuration',
+    plugins: {
+      import: eslintPluginImport,
+    },
+    rules: {
+      ...eslintPluginImport.flatConfigs.typescript.rules,
+      ...typescriptRules,
+    },
+    name: '@mediamonks/eslint-config / typescript configuration',
   },
 ];
 
 /** @type {import('@typescript-eslint/utils').TSESLint.FlatConfig.ConfigFile} */
 const typescriptReact = [
   {
-    ...eslintPluginImport.flatConfigs.react,
-    files: ['**/*.ts', '**/*.tsx'],
-    name: '@mediamonks/eslint-config / eslint-plugin-import / .tsx configuration',
-  },
-  {
-    ...typescriptReactRecommended,
-    files: ['**/*.ts', '**/*.tsx'],
-    name: '@mediamonks/eslint-config / typescript react recommended configuration',
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    settings: {
+      'import/extensions': ['.ts', '.tsx', '.mts', '.cts'],
+    },
+    rules: typescriptReactRules,
+    name: '@mediamonks/eslint-config / typescript react configuration',
   },
 ];
 
-const overrides = {
-  rules: {
-    'import/no-unresolved': 'off',
+/** @type {import('eslint').Linter.Config} */
+const overrides = [
+  {
+    plugins: {
+      import: eslintPluginImport,
+    },
+    rules: {
+      'import/no-unresolved': 'off',
+    },
+    name: '@mediamonks/eslint-config / overrides',
   },
-};
+];
+
+/** @type {import('eslint').Linter.Config} */
+const base = [
+  eslintPluginSecurity.configs.recommended,
+
+  ...javascript,
+
+  // The ESLint Prettier config will disable rules that conflict with the Prettier code style
+  eslintConfigPrettier,
+];
 
 export const configs = {
   /** @type {import('eslint').Linter.Config} */
-  javascript: [...javascript, overrides],
+  javascript: [...base, ...overrides],
   /** @type {import('eslint').Linter.Config} */
-  react: [...javascript, ...react, overrides],
+  react: [...base, ...react, ...storybook, ...overrides],
   /** @type {import('@typescript-eslint/utils').TSESLint.FlatConfig.ConfigArray} */
-  typescript: [...javascript, ...typescript, overrides],
+  typescript: [...base, ...typescript, ...overrides],
   /** @type {import('@typescript-eslint/utils').TSESLint.FlatConfig.ConfigArray} */
-  typescriptReact: [...javascript, ...typescript, ...typescriptReact, ...react, overrides],
+  typescriptReact: [
+    ...base,
+    ...typescript,
+    ...typescriptReact,
+    ...react,
+    ...storybook,
+    ...overrides,
+  ],
 };
--- src/typescriptReact.js
+++ src/typescriptReact.js
@@ -1,45 +1,40 @@
-/**
- * @type {import('eslint').Linter.Config}
- */
-export const recommended = {
-  name: '@mediamonks/eslint-config/typescript-react/recommended',
-  rules: {
-    '@typescript-eslint/naming-convention': [
-      'error',
-      {
-        selector: 'default',
-        format: ['strictCamelCase'],
-        leadingUnderscore: 'allow',
-        trailingUnderscore: 'forbid',
+/** @type {import('eslint').Linter.Config['rules']} */
+export const rules = {
+  '@typescript-eslint/naming-convention': [
+    'error',
+    {
+      selector: 'default',
+      format: ['strictCamelCase'],
+      leadingUnderscore: 'allow',
+      trailingUnderscore: 'forbid',
+    },
+    {
+      selector: 'import',
+      format: ['camelCase', 'PascalCase'],
+    },
+    {
+      selector: ['variable', 'function'],
+      format: ['StrictPascalCase', 'strictCamelCase'],
+      leadingUnderscore: 'allow',
+      trailingUnderscore: 'forbid',
+    },
+    {
+      selector: ['typeLike', 'enumMember'],
+      format: ['StrictPascalCase'],
+      custom: {
+        regex: '^[A-Z]{2}',
+        match: false,
       },
-      {
-        selector: 'import',
-        format: ['camelCase', 'PascalCase'],
+      leadingUnderscore: 'forbid',
+      trailingUnderscore: 'forbid',
+    },
+    {
+      selector: 'objectLiteralProperty',
+      format: [],
+      filter: {
+        regex: '^--',
+        match: true,
       },
-      {
-        selector: ['variable', 'function'],
-        format: ['StrictPascalCase', 'strictCamelCase'],
-        leadingUnderscore: 'allow',
-        trailingUnderscore: 'forbid',
-      },
-      {
-        selector: ['typeLike', 'enumMember'],
-        format: ['StrictPascalCase'],
-        custom: {
-          regex: '^[A-Z]{2}',
-          match: false,
-        },
-        leadingUnderscore: 'forbid',
-        trailingUnderscore: 'forbid',
-      },
-      {
-        selector: 'objectLiteralProperty',
-        format: [],
-        filter: {
-          regex: '^--',
-          match: true,
-        },
-      },
-    ],
-  },
+    },
+  ],
 };
